840fdb0cc5beebee770e79dc9f009e7cb356667e
AR-Shahin <mdshahinmije96@gmail.com>
2025-01-05 20:46:36 +0600

    h

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -30,7 +30,7 @@
     <div class="card">
         <div class="card-body">
             <h5>এই মাসের সেল : <span class="text-success">{{ convert_eng_to_bn_number($currentMonthSell) }}</span></h5>
-            <h5>টোটাল : <span class="text-success">{{ convert_eng_to_bn_number($totalPrice) }} টাকা ({{ bn_to_en($totalPrice) }})</span></h5>
+            <h5>টোটাল : <span class="text-success">{{ convert_eng_to_bn_number($totalPrice) }} টাকা (<small>{{ bn_to_en($totalPrice) }}</small>)</span></h5>
         </div>
 
         <table class="table-sm table table-bordered text-center">
