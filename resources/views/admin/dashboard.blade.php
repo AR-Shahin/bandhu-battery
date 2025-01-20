1544063a280e6a78f4a478e770b4419fefc3b341
AR-Shahin <mdshahinmije96@gmail.com>
2025-01-20 19:44:21 +0600

    up

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -29,9 +29,9 @@
          <div class="col-lg-3 col-6" bis_skin_checked="1">
             <div class="small-box bg-secondary" bis_skin_checked="1">
                <div class="inner" bis_skin_checked="1">
-                  <h4>{{ convert_eng_to_bn_number(($products['totalAmountOfMoney'] + $currentMonthSell)) }} TK</h4>
+                  <h4>{{ convert_eng_to_bn_number(($products['totalAmountOfMoney'] + $totalPrice)) }} TK</h4>
                   {{-- <p>{{ convertNumberToBanglaWords($products['totalAmountOfMoney'])}} টাকা</p> --}}
-                  <p>{{ bn_to_en(($products['totalAmountOfMoney'] + $currentMonthSell))}} টাকা</p>
+                  <p>{{ bn_to_en(($products['totalAmountOfMoney'] + $totalPrice))}} টাকা</p>
                </div>
                <div class="icon" bis_skin_checked="1">
                   <i class="ion ion-bag"></i>
